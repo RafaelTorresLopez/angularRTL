f63da772384388ca3861008eebeecf7ffe66d58f
RafaelTorresLopez <rtorres8@gmail.com>
2020-06-09 18:58:15 +0200

    Llamada a Listado de Usuarios

--- practica/src/app/services/list-users.service.ts
+++ practica/src/app/services/list-users.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListUsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  public listUsers() {
+
+    let result:string;
+
+    console.log ("Llamando al servicio de users en regres.in...")
+
+    this.http.get('https://reqres.in/api/users?page=1').subscribe(data => {
+      console.log(data);      
+    })
+  }
+
+
 }
--- practica/src/app/view/wellcome/wellcome.component.ts
+++ practica/src/app/view/wellcome/wellcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListUsersService } from 'src/app/services/list-users.service';
 
 @Component({
   selector: 'app-wellcome',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WellcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private listUsersService : ListUsersService) { }
 
   ngOnInit(): void {
   }
+  
+  listUsers() {
+    this.listUsersService.listUsers();
+  }
+
+  listResources() {
+
+  }
+
+
 
 }
